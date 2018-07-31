a095facee975177cb328901d6eff52cdf2e6f49e
VitozzRDX <bednovv@mail.ru>
2018-07-31 17:10:07 +0300

    adding a server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html');
+});
+
+
+
+server.listen(process.env.PORT || 8081,function(){
+    console.log('Listening on '+server.address().port);
+});
