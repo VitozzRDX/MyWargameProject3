46041a2f272e1f8f558939314a291cec7e5f5690
VitozzRDX <bednovv@mail.ru>
2018-07-31 17:44:15 +0300

    adding app.use to  a server

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 
+app.use('/fabric.js-2.2.3',express.static(__dirname + '/fabric.js-2.2.3'));
+app.use('/imag',express.static(__dirname + '/imag'));
+app.use('/script',express.static(__dirname + '/script'));
+
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
 });
